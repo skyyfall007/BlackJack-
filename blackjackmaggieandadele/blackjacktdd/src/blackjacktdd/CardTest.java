d4d5892002b8a2eb25ffbf704dcd2ae39aa355d8
Maggie <mmuse@ggc.edu>
2017-04-26 19:13:53 -0400

    fixed project setup and imports

--- blackjackmaggieandadele/blackjacktdd/src/blackjacktdd/CardTest.java
+++ blackjackmaggieandadele/blackjacktdd/src/blackjacktdd/CardTest.java
@@ -2,31 +2,33 @@
  * 
  */
 package blackjacktdd;
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
-/**
- * 
- * @author SKYFALL (Adele Francois)
+
+/*@author SKYFALL (Adele Francois)
  * @author Maggie Muse
  * @collborativeaid John Rose
  */
 
 public class CardTest 
 {
-	private DeckOfCards test_deck;
+	private CardDeck test_deck;
 	
 	@Before
 	public void setup()
 	{
-		test_deck= new DeckOfCards();
+		test_deck= new CardDeck();
 	}
 	/**
 	 * Test to confirm that deck has 52 cards
 	 */
 	@Test
+	
 	public void testDeckOfCards()
 	{
-		assertEquals(52, test_deck.sizeofDeck());
+		assertEquals(52, test_deck.sizeOfDeck());
 	}
 }
